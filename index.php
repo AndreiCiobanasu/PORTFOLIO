cb6366cec34e7a5311c74274df10abf32920c446
AndreiCiobanasu <a_ciobanasu@yahoo.fr>
2017-10-12 20:46:49 +0200

    bla

--- index.php
+++ index.php
@@ -225,7 +225,7 @@
 			</div>
 		</footer>
 
-git 
+
 		<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script type="text/javascript" src="js/javascript.js"></script>
 
