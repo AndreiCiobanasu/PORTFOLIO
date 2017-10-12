9afc6dfcbe601fb48f84e18038e47ec9d86dfa13
AndreiCiobanasu <a_ciobanasu@yahoo.fr>
2017-10-12 20:41:09 +0200

    modifications

--- index.php
+++ index.php
@@ -223,10 +223,9 @@
 				<div><a class="facebook wow fadeIn" data-wow-duration="2s" data-wow-delay="2" target="_blank" href="http://facebook.com">Facebook</a></div>
 				<div><a class="e-mail wow fadeIn" data-wow-duration="3s" data-wow-delay="3" href="mailto:a_ciobanasu@yahoo.fr">E-mail</a></div>
 			</div>
-			<div></div>
 		</footer>
 
-
+git 
 		<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script type="text/javascript" src="js/javascript.js"></script>
 
