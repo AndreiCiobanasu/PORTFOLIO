37cdfca445e141288795722406ccf50f6efda605
AndreiCiobanasu <a_ciobanasu@yahoo.fr>
2018-02-21 11:26:15 +0100

    Creation de la présentation de mon Portfolio

--- js/javascript.js
+++ js/javascript.js
@@ -30,7 +30,57 @@ $(document).ready(function () {
             }
         }
     );
-
-
-
 });
+
+/*
+$(function() {
+            $(window).scroll(function() {
+                if ($(this).scrollTop() >= 100) {
+                    $('.nom').fadeOut(1000); // ici la classe menu .menu représente donc mon menu qui dois disparaître au scrollbottom et réapparaître au scrolltop
+ 
+                }else {
+                    $(".nom").fadeIn(1000);
+                }
+            });
+ 
+        });*/
+
+(function() {
+    var documentElem = $(document),
+        nav =  $('nav'),
+        lastScrollTop = 0;
+
+    documentElem.on('scroll', function() {
+        var currentScrollTop = $(this).scrolltop();
+
+        if (currentScrollTop > lastScrollTop) nav.addClass('hidden');
+
+        else nav.removeClass('hidden');
+
+        lastScrollTop = currentScrollTop;
+    });
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
